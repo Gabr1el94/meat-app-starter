c95db24367c1e04a451dcce6819498344be16773
Gabr1el94 <bielsystem64@gmail.com>
2023-12-14 07:39:49 -0300

    config init for backend security

---
+++ backend/server.ts
@@ -0,0 +1,25 @@
+import * as jsonServer from 'json-server'
+import {Express} from 'express';
+
+import * as fs from 'fs'
+import * as https from 'https'
+
+const server: Express = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+
+// get files security for https
+const options = {
+  cert: fs.readFileSync('./backend/keys/cert.pem'),
+  key: fs.readFileSync('./backend/keys/key.pem')
+}
+
+server.use(router)
+https.createServer(options, server).listen(3001, () => {
+  console.log('JSON Server is running on port 3001')
+})
