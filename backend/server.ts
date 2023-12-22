99195925d29cfb318078d978f7d03953d9e15bf8
Gabr1el94 <bielsystem64@gmail.com>
2023-12-22 07:11:57 -0300

    add jwt and middlware with authorization

--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs'
 import * as https from 'https'
 
 import { handleAuthentication } from './auth';
+import { handleAuthorization } from './authz';
 
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -23,6 +24,7 @@ const options = {
 
 //routes for Login
 server.post('/login', handleAuthentication)
+server.use('/orders', handleAuthorization)
 
 server.use(router)
 https.createServer(options, server).listen(3001, () => {
