1a38dbce847f89c14bb3eb3cb7de4d3e44d8a3d2
Gabr1el94 <bielsystem64@gmail.com>
2024-01-24 07:30:39 -0300

    add canActive

--- src/app/security/loggedin.guard.ts
+++ src/app/security/loggedin.guard.ts
@@ -1,19 +1,28 @@
 import { Injectable } from "@angular/core";
-import { CanLoad, Route } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { LoginService } from "./login/login.service";
 
 @Injectable()
-export class LoggedInGuard implements CanLoad{
+export class LoggedInGuard implements CanLoad, CanActivate{
 
     constructor(private loginService: LoginService){}
 
-    canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+
+    checkAuthentication(path: string):boolean{
         const loggedIn = this.loginService.isLoggedIn();
         if (!loggedIn){
-            this.loginService.handleLogin(`/${route.path}`);
+            this.loginService.handleLogin(`/${path}`);
         }
         return loggedIn;
+    }
+
+    canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+       return this.checkAuthentication(route.path);
+    }
+
+    canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean | Observable<boolean>{
+        return this.checkAuthentication(activatedRoute.routeConfig.path);
     }
 
 }
