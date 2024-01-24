2bcfec573c9324cebccb9a1be3151131a1d884f5
Gabr1el94 <bielsystem64@gmail.com>
2024-01-24 06:56:11 -0300

    add implement Guard routes with CanLoad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,11 @@ import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component"
 import { OrderSummaryComponent } from "./orderr-summary/orderr-summary.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { LoginComponent } from "./security/login/login.component";
+import { LoggedInGuard } from "./security/loggedin.guard";
 
 export const ROUTES : Routes = [
     {path:'', component: HomeComponent},
+    {path:'login/:to', component: LoginComponent},
     {path:'login', component: LoginComponent},
     {path:'restaurants', component: RestaurantsComponent},
     {path:'restaurants/:id', component: RestaurantDetailComponent, children: [
@@ -17,7 +19,8 @@ export const ROUTES : Routes = [
         {path:'menu', component: MenuComponent},
         {path:'reviews', component: ReviewsComponent}
     ]},
-    {path:'order', loadChildren: './order/order.module#OrderModule'},
+    {path:'order', loadChildren: './order/order.module#OrderModule',
+        canLoad: [LoggedInGuard]},
     {path:'order-summary', component: OrderSummaryComponent},
     {path:'about', loadChildren: './about/about.module#AboutModule'},
     {path:'**', component: NotFoundComponent},
---
+++ src/app/security/loggedin.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { CanLoad, Route } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import { LoginService } from "./login/login.service";
+
+@Injectable()
+export class LoggedInGuard implements CanLoad{
+
+    constructor(private loginService: LoginService){}
+
+    canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+        const loggedIn = this.loginService.isLoggedIn();
+        if (!loggedIn){
+            this.loginService.handleLogin(`/${route.path}`);
+        }
+        return loggedIn;
+    }
+
+}
